22bf8c68f33aaec20f8db2499578db2287a44d1e
RafiMoldovsky <moldysrm@gmail.com>
2022-12-24 14:55:24 -0800

    Initial Commit

---
+++ TodoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoListApp
+//
+//  Created by Rafi Moldovsky on 12/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
