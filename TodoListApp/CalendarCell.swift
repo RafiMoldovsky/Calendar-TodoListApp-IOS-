176776c5ff251a2cfd8f6bd57f9fead91ef148a8
RafiMoldovsky <moldysrm@gmail.com>
2022-12-24 17:13:01 -0800

    Basic setup for monthly calendar

---
+++ TodoListApp/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  TodoListApp
+//
+//  Created by Rafi Moldovsky on 12/24/22.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
